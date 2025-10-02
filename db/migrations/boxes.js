071b94841f14e423ce568bc00781dfb6065357c6
DesperadoUa <lazarevonstant@gmail.com>
2025-10-02 07:36:25 +0300

    Add migration for shares box bonuses

---
+++ db/migrations/boxes.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  return knex.schema.createTable("boxes", (table) => {
+    table.increments("id").primary()
+    table.string("title").notNullable()
+    table.string("subTitle").defaultTo("")
+    table.decimal("depositAmount", 15, 2).notNullable()
+    table.integer("order").defaultTo(0)
+    table.string("image").defaultTo("")
+    table.timestamp("created_at").defaultTo(knex.fn.now())
+    table
+      .timestamp("updated_at")
+      .defaultTo(knex.raw("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+  })
+}
+
+exports.down = async function (knex) {
+  return knex.schema.dropTable("boxes")
+}
